6d7b42b7479f7a9e44be8b90ec26277913dad235
Stephan Smola <smola@farbspender.de>
2022-12-26 19:51:42 +0100

    For Loops

--- bbbasic/src/interpreter.rs
+++ bbbasic/src/interpreter.rs
@@ -1,13 +1,13 @@
 extern crate pest;
 
+use std::arch::x86_64::_mm_pause;
 use pest::iterators::{Pair, Pairs};
-use pest::Parser;
+use pest::{Parser, RuleType};
 
 pub mod bb_types;
 
 use bb_types::BbInt;
 use bb_types::BbFloat;
-
 
 
 #[derive(Parser)]
@@ -18,40 +18,52 @@ pub struct BBasicParser;
 #[derive(Debug)]
 pub enum InterpreterError {
     Generic(String),
+    Syntax
 
 }
 
+#[derive(Debug)]
 pub enum BBExpression {
     String(String),
     Integer(BbInt),
-    Float(BbFloat)
+    Float(BbFloat),
 }
 
 pub type BBBlock = Vec<BBStatement>;
 
+#[derive(Debug)]
 pub struct BBAssignment {
     name: String,
-    value: BBExpression
+    value: BBExpression,
+}
+
+
+#[derive(Debug)]
+pub enum BBNumeric {
+    Float(BbFloat),
+    Integer(BbInt)
 }
 
 #[derive(Debug)]
 pub enum BBStatement {
     PRINT(BBExpression),
-    FORLoop(BBAssignment, BBBlock),
+    FOR(BBAssignment, BBExpression),
+    NEXT(String),
+    END,
 
     Nop,
 }
 
 
-fn interpret_expression(pairs: Pairs<Rule>) -> Result<BBExpression, InterpreterError> {
-    for p in pairs {
+fn interpret_expression(pair: Pair<Rule>) -> Result<BBExpression, InterpreterError> {
+    for p in pair.into_inner() {
         return match p.as_rule() {
             Rule::bb_float_literal => Ok(BBExpression::Float(p.as_str().parse::<BbFloat>().unwrap())),
             Rule::bb_int_literal => Ok(BBExpression::Integer(p.as_str().parse::<BbInt>().unwrap())),
             Rule::bb_string => Ok(BBExpression::String(p.as_str().to_string())),
 
             _ => {
-                println!("{:?}", p);
+                println!("ERROR on Expression {:?}", p);
                 Err(InterpreterError::Generic("What do I know".to_string()))
             }
         }
@@ -60,16 +72,63 @@ fn interpret_expression(pairs: Pairs<Rule>) -> Result<BBExpression, InterpreterE
     return Err(InterpreterError::Generic("Empty Expression".to_string()))
 }
 
-fn interpret_for_loop(pair: Pair<Rule>) -> Result<BBStatement, InterpreterError> {
+
+fn interpret_assignment(pair: Pair<Rule>) -> Result<BBAssignment, InterpreterError> {
+    let mut pairs = pair.into_inner();
+
+    let var_name = pairs.next().unwrap();
+    let value = interpret_expression(pairs.next().unwrap());
+
+    return match value {
+        Ok(e) => Ok(BBAssignment { name: var_name.as_str().to_string(), value: e }),
+        Err(e) => Err(e)
+    }
+}
+
+fn interpret_for_statement(pair: Pair<Rule>) -> Result<BBStatement, InterpreterError> {
+    if Rule::bb_for_statement == pair.as_rule() {
+        let mut pairs = pair.into_inner();
+
+        let a = pairs.next().unwrap();
+
+        let b = pairs.next().unwrap();
+
+        return Ok(BBStatement::FOR(
+            interpret_assignment(a).unwrap(),
+            interpret_expression(b).unwrap()
+        ))
+    }
+
     Err(InterpreterError::Generic("Not a for loop".to_string()))
+}
+
+fn interpret_next_statement(pair: Pair<Rule>) -> Result<BBStatement, InterpreterError> {
+    if Rule::bb_next_statement == pair.as_rule() {
+        return Ok(BBStatement::NEXT(pair.into_inner().next().unwrap().as_str().to_string()));
+    }
+
+    Err(InterpreterError::Syntax)
+}
+
+
+fn interpret_print_statement(pair: Pair<Rule>) -> Result<BBStatement, InterpreterError> {
+    if Rule::bb_print_statement == pair.as_rule() {
+        return Ok(BBStatement::PRINT(interpret_expression(pair.into_inner().next().unwrap()).unwrap()))
+    }
+
+    return Err(InterpreterError::Syntax);
 }
 
 fn interpret_statement(pair: Pair<Rule>) -> Result<BBStatement, InterpreterError> {
 
     return match pair.as_rule() {
-        Rule::bb_print_statement => Ok(BBStatement::PRINT(interpret_expression(pair.into_inner()).unwrap())),
+        Rule::bb_print_statement => interpret_print_statement(pair),
 
-        Rule::bb_for_statement => interpret_for_loop(pair),
+        Rule::bb_for_statement => interpret_for_statement(pair),
+
+        Rule::bb_next_statement => interpret_next_statement(pair),
+
+        Rule::bb_end_statement => Ok(BBStatement::END),
 
         Rule::EOI => Ok(BBStatement::Nop),
 
@@ -81,9 +140,10 @@ fn interpret_statements(pairs: Pairs<Rule>) -> Result<Vec<BBStatement>, Interpre
     let mut r: Vec<BBStatement> = Vec::new();
 
     for pair in pairs {
+        println!("{:?}", pair);
         match interpret_statement(pair) {
             Ok(s) => r.push(s),
-            Err(e) => return Err(e)
+            Err(e) => {}
         }
     }
 
@@ -100,6 +160,9 @@ pub fn interpret_program(pair: Pair<Rule>) -> Result<Vec<BBStatement>, Interpret
 pub fn interpret(input: &str) -> Result<Vec<BBStatement>, InterpreterError> {
 
     let r = BBasicParser::parse(Rule::bb_program, input);
+
+    println!("Interpreting");
+    println!("{}", input);
 
     match r {
         Ok(r) => {
--- bbbasic/src/main.rs
+++ bbbasic/src/main.rs
@@ -7,8 +7,12 @@ pub mod interpreter;
 use interpreter::BBStatement;
 use interpreter::interpret;
 use interpreter::BBExpression;
+use crate::interpreter::{BBAssignment, BBBlock};
 
 
+fn run_for_loop(assignment: &BBAssignment, body: &BBBlock) {
+    println!("A for loop");
+}
 
 
 fn run(statements: &Vec<BBStatement>) {
@@ -17,12 +21,17 @@ fn run(statements: &Vec<BBStatement>) {
         match s {
             BBStatement::PRINT(e) => {
                 match e {
-                    BBExpression::String(s) => println!("{s}"),
-                    BBExpression::Integer(i) => println!("{i}"),
-                    BBExpression::Float(f) => println!("{f}")
+                    BBExpression::String(s) => println!("{}", s),
+                    BBExpression::Integer(i) => println!("{}", i),
+                    BBExpression::Float(f) => println!("{}", f)
                 }
-            }
+            },
+
+            // BBStatement::FOR(a, b) => run_for_loop(a, b),
+
             BBStatement::Nop => {}
+
+            _ => {}
         }
     }
 }
@@ -30,12 +39,22 @@ fn run(statements: &Vec<BBStatement>) {
 
 fn main() {
 
-    let res = interpret("PRINT 12");
+    let inp =
+"FOR i=1 TO 10
+    PRINT 12
+    NEXT i
+END";
 
-    match res {
-        Ok(statements) => run(&statements),
-        Err(e) => println!("{:?}", e)
+    let res = interpret(inp);
+
+    for r in res {
+        println!("{:?}", r);
     }
+
+    // match res {
+    //     Ok(statements) => run(&statements),
+    //     Err(e) => println!("{:?}", e)
+    // }
 }
 
 
