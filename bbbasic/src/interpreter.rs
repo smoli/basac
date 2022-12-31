0cf2a2caf2e934bd22566124a1c235eb0e7008ec
Stephan Smola <smola@farbspender.de>
2022-12-31 07:59:22 +0100

    Number Literal

--- bbbasic/src/interpreter.rs
+++ bbbasic/src/interpreter.rs
@@ -1,24 +1,72 @@
+use std::collections::HashMap;
 use std::io::Write;
-use crate::parser::{PrintStatement, PrintStatement_literal, Program, Statement};
+use crate::parser::{Block, NumberLiteral, NumberLiteral_value, PrintStatement, PrintStatement_literal, Program, Statement};
 
 
-fn execute_print(s: &PrintStatement, stdout: &mut impl Write) {
+#[derive(Debug,Clone)]
+enum Value {
+    String(String),
+    Integer(i64),
+    Float(f64),
+    Boolean(bool)
+}
+
+#[derive(Debug)]
+struct Scope {
+    values: HashMap<String, Value>
+}
+
+impl Scope {
+
+    fn new() ->Scope {
+        Scope { values: HashMap::new() }
+    }
+
+    fn set(&mut self, name: &String, value: Value) {
+        self.values.insert(name.clone(), value);
+    }
+
+    fn get(&self, name: &String) -> Option<&Value> {
+        self.values.get(name)
+    }
+
+}
+
+
+fn execute_print(s: &PrintStatement, scope: &mut Scope, stdout: &mut impl Write) {
     match &s.literal {
-        PrintStatement_literal::IntegerLiteral(i) => {
-            let _ = stdout.write_all(format!("{}", i).as_bytes());
-        }
+        PrintStatement_literal::NumberLiteral(n) => {
+            match &n.value {
+                NumberLiteral_value::FloatLiteral(f) => {
+                    let _ = stdout.write_all(format!("{}", f).as_bytes());
+                }
+                NumberLiteral_value::IntegerLiteral(i) => {
+                    let _ = stdout.write_all(format!("{}", i).as_bytes());
+                }
+            }
+        },
+
         PrintStatement_literal::StringLiteral(s) => {
             let _ = stdout.write_all(s.body.as_bytes());
         }
     };
 }
 
-pub fn execute(program: Program, stdout: &mut impl Write) {
-
-    for s in program.body.statements  {
-        match s {
-            Statement::EndStatement(_) => {}
-            Statement::PrintStatement(s) => execute_print(&s, stdout)
-        }
+fn execute_statement(statement: &Statement, scope: &mut Scope, stdout: &mut impl Write) {
+    match statement {
+        Statement::EndStatement(_) => {}
+        Statement::PrintStatement(s) => execute_print(s, scope, stdout)
     }
+}
+
+fn execute_block(block: &Block, scope: &mut Scope, stdout: &mut impl Write ) {
+    for i in 0..block.statements.len() {
+        let s = block.statements.get(i).unwrap();
+            execute_statement(s, scope, stdout);
+    }
+}
+
+pub fn execute(program: &Program, stdout: &mut impl Write) {
+    let mut scope = Scope::new();
+    execute_block(&program.body, &mut scope, stdout);
 }
--- bbbasic/tests/print.rs
+++ bbbasic/tests/print.rs
@@ -11,7 +11,7 @@ fn print_a_string() {
     let r = parser::Program::parse("PRINT \"Hello, World!\"").expect("Parse failed");
 
     println!("{:?}", r);
-    execute(r, &mut out);
+    execute(&r, &mut out);
 
     assert_eq!(out.into_inner(), exp.into_inner())
 }
@@ -23,7 +23,19 @@ fn print_an_integer() {
     let r = parser::Program::parse("PRINT 12").expect("Parse failed");
 
     println!("{:?}", r);
-    execute(r, &mut out);
+    execute(&r, &mut out);
+
+    assert_eq!(out.into_inner(), exp.into_inner())
+}
+
+#[test]
+fn print_a_float() {
+
+    let (mut out, exp) = common::make_buffer("12.012");
+    let r = parser::Program::parse("PRINT 12.012").expect("Parse failed");
+
+    println!("{:?}", r);
+    execute(&r, &mut out);
 
     assert_eq!(out.into_inner(), exp.into_inner())
 }
