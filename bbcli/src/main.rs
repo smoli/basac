0228f50986c715b92ad690bcce355184d88ddc90
Stephan Smola <smola@farbspender.de>
2023-01-03 19:40:23 +0100

    Simple Benchmarking functionality for the CLI

--- bbcli/src/main.rs
+++ bbcli/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::time::Instant;
 use clap::builder::Str;
 use bbbasic;
 use clap::Parser as Clapper;
@@ -6,22 +7,60 @@ use clap::Parser as Clapper;
 #[derive(Clapper, Debug)]
 #[clap(author = "Stephan Smola", version = "0.1", about = "Basic Interpreter inspired by BBC Basic", long_about = None)]
 struct Args {
+
+    /// Run as benchmark.
+    #[clap(short = 'b', long, action)]
+    benchmark: bool,
+
+    /// Benchmark repetitions
+    #[clap(short = 'c', long, default_value = "20")]
+    count: u32,
+
+
+
     /// Filename of a .bbb-File
     filename: String
+
 }
 
 
+fn benchmark(filename: &String, count: u32) {
+    match fs::read_to_string(filename) {
+        Ok(code) => {
+            let t1 = Instant::now();
+            for _ in 0..count {
+                bbbasic::execute(code.as_str());
+            }
+            let elapsed = t1.elapsed();
 
-fn main() {
-    let args = Args::parse();
+            println!("Running {} {} times:", filename, count);
+            println!("Total time:\t\t{:.2?}", elapsed);
+            println!("Avg. single exec:\t{:.2?}", elapsed / count);
 
-    match fs::read_to_string(&args.filename) {
+        }
+        Err(e) => {
+            eprintln!("File not found: {}", filename);
+        }
+    }
+}
+
+fn run(filename: &String) {
+    match fs::read_to_string(filename) {
         Ok(code) => {
             bbbasic::execute(code.as_str());
         }
         Err(e) => {
-            eprintln!("File not found: {}", args.filename);
+            eprintln!("File not found: {}", filename);
         }
     }
+}
 
+fn main() {
+    let args = Args::parse();
+
+    if args.benchmark {
+        benchmark(&args.filename, args.count)
+    } else {
+        run(&args.filename);
+    }
 }
